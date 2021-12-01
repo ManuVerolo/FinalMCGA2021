a1093717fdd4cb9636605f75663515f963d116f6
ManuVerolo} <manuelverolo@gmail.com>
2021-12-01 23:16:42 +0100

    Agrego ruta get pruducts

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import productsRoutes from './routes/products.js';
+
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
+app.use('/products', productsRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://ManuVerolo:manuel123@cluster0.kg1go.mongodb.net/prueba?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
