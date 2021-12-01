77c79eae1783b2376f2fb2cc6e27a4ed6079d16c
ManuVerolo} <manuelverolo@gmail.com>
2021-12-01 23:20:51 +0100

    Agrego metodo y ruta create product

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -13,3 +13,18 @@ export const getProducts = async (req, res) => {
         res.status(404).json({ message: error });
     }
 }
+
+export const createProduct = async (req, res) => {
+    const { name , description, selectedFile, price } = req.body;
+
+    const newProduct = new Products({ name, description, selectedFile, price })
+
+    try {
+        await newProduct.save();
+
+        res.status(201).json(newProduct );
+    } catch (error) {
+        res.status(409).json({ message: error });
+    }
+}
+
