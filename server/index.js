74d6e1efc73e73a1ed0e1d330baca680bcbbfb30
ManuVerolo} <manuelverolo@gmail.com>
2021-12-02 21:20:17 +0100

    Agrego .env y .env-example

--- server/index.js
+++ server/index.js
@@ -3,18 +3,17 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import productsRoutes from './routes/products.js';
-
+import dotenv from 'dotenv';
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/products', productsRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://ManuVerolo:manuel123@cluster0.kg1go.mongodb.net/prueba?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
         .catch(err => console.log(err));
