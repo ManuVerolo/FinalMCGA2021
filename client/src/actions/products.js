658100fdbf7f791881c848b3b0130e334b33aae2
ManuVerolo} <manuelverolo@gmail.com>
2021-12-02 21:13:07 +0100

    Agrego constantes

--- client/src/actions/products.js
+++ client/src/actions/products.js
@@ -1,10 +1,11 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 import * as api from '../api';
 
 //ACTION CREATORS
 export const getProducts = () => async (dispatch) =>  {
     try{
         const { data } = await api.fetchProducts();
-        dispatch({ type: 'FETCH_ALL', payload: data });
+        dispatch({ type: FETCH_ALL, payload: data });
     }catch (error){
         console.log(error);
 
@@ -14,7 +15,7 @@ export const getProducts = () => async (dispatch) =>  {
 export const createProduct = (product) => async (dispatch) => {
     try{
         const { data } = await api.createProduct(product);
-        dispatch({type: 'CREATE', payload: data});
+        dispatch({type: CREATE, payload: data});
     }catch (error){
         console.log(error);
     }
@@ -23,7 +24,7 @@ export const createProduct = (product) => async (dispatch) => {
 export const updateProduct = (id, product) => async (dispatch) => {
     try{
         const {data} = await api.updateProduct(id, product);
-        dispatch({type: 'UPDATE', payload: data});
+        dispatch({type: UPDATE, payload: data});
     }catch (error){
         console.log(error);
     }
@@ -32,7 +33,7 @@ export const updateProduct = (id, product) => async (dispatch) => {
 export const deleteProduct = (id) => async (dispatch) => {
     try{
         await api.deleteProduct(id);
-        dispatch({type: 'DELETE', payload: id});
+        dispatch({type: DELETE, payload: id});
     }catch (error){
         console.log(error);
     }
--- client/src/reducers/products.js
+++ client/src/reducers/products.js
@@ -1,13 +1,14 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 export default (products = [], action) => {
     switch(action.type) {
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             return [...products, action.payload];
-        case 'UPDATE':
+        case UPDATE:
             //Actualizo sino la devuelvo como estaba
             return products.map((product) => product.__id === action.payload.__id ? action.payload : product);
-        case 'DELETE':
+        case DELETE:
             //Mantengo los productos menos el que se elimina
             return products.filter((product) => product._id !== action.payload);
         default:
